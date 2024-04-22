343df17106cbf47fe7ffcf8af228f0bcc0411dd0
Morais Lendengue <moraislenf@hotmail.com>
2024-04-21 23:01:48 -0300

    - Adição das classes: Servico, Cliente, Boleto e seus respectivos relacionamentos

---
+++ Domain/Back.PagLigeiro.Domain/Model/Boleto/BoletoModel.cs
@@ -0,0 +1,28 @@
+﻿using Back.PagLigeiro.Domain.Enum;
+using Back.PagLigeiro.Domain.Model.Cliente;
+using Back.PagLigeiro.Domain.Model.Servico;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Back.PagLigeiro.Domain.Model.Boleto
+{
+    [Table("TAB_BOLETO")]
+    public class BoletoModel : Base
+    {
+        [MaxLength(300)]
+        public string Observacao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataMinPagamento { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public StatusBoletoEnum Status { get; set; }
+
+        #region Relacionamentos
+        public int IdCliente { get; set; }
+        public ClienteModel Cliente { get; set; }
+
+        public int IdServico { get; set; }
+        public ServicoModel Servico { get; set; }
+        #endregion
+    }
+}
--- Domain/Back.PagLigeiro.Domain/Model/User/UserModel.cs
+++ Domain/Back.PagLigeiro.Domain/Model/User/UserModel.cs
@@ -1,5 +1,8 @@
 ﻿using Back.PagLigeiro.Domain.Enum;
+using Back.PagLigeiro.Domain.Model.Cliente;
+using Back.PagLigeiro.Domain.Model.Servico;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -13,16 +16,29 @@ namespace Back.PagLigeiro.Domain.Model.User
 
         [MaxLength(300)]
         public string Email { get; set; }
-        public DateTime DataNascimento { get; set; }
+
+        public DateTime? DataNascimento { get; set; }
+
         [MaxLength(11)]
         public string CPF { get; set; }
+
         [MaxLength(20)]
         public string RG { get; set; }
+
         [MaxLength(500)]
         public string Endereco { get; set; }
-        [MaxLength(50)]
+
+        [MaxLength(64)]
         public string Password { get; set; }
+
         public RoleUserEnum Role { get; set; }
-        public DateTime LastAccess { get; set; }
+        public DateTime? LastAccess { get; set; }
+
+        #region Relacionamentos
+
+        public List<ServicoModel> Servicos { get; set; }
+        public List<ClienteModel> Clientes { get; set; }
+
+        #endregion Relacionamentos
     }
 }
